90f6116778b45351742dfa3fad4d19f8ae69decf
shadow <lightshadow416@gmail.com>
2024-03-18 17:49:43 +0100

    project checkpoint {2}

--- core.js
+++ core.js
@@ -1,16 +1,8 @@
-export default function useSound(element, src, action) {
+export default function soundEffect(src) {
     let audio = new Audio(src);
-    let selectedElement = document.querySelector(element);
     try {
-        selectedElement.addEventListener(action, () => {
-            audio.play();
-        });
+        audio.play();
     } catch (error) {
         console.error(`An error occurred: ${error}`);
     }
-    return () => {
-        document.removeEventListener(action, () => {
-            audio.play();
-        });
-    };
 }
